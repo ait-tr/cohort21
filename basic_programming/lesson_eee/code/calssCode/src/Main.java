1f363e7e0995a81805c1a602bb4a5445c315e566
dennisbrovarnyy <denis@brovarnyy.com>
2022-11-18 20:12:40 +0100

    lesson 555

---
+++ basic_programming/lesson_eee/code/calssCode/src/Main.java
@@ -0,0 +1,10 @@
+import de.aittr.controller.Books;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Books b1 = new Books(12, "Hui vam");
+        System.out.println(b1);
+    }
+}
